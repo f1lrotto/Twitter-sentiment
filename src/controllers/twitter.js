6c695c806c4b27d72c26803293145b0c83849c34
f1lrotto <filip.hupka@gmail.com>
2022-06-13 02:15:29 +0200

    added query options

--- src/controllers/twitter.js
+++ src/controllers/twitter.js
@@ -1,6 +1,6 @@
 async function getTweets(client, keyword, limit) {
   const result = await client.get("tweets/search/recent", {
-    query: `${keyword} OR #${keyword} lang:en -is:retweet -is:reply`,
+    query: `${keyword} OR #${keyword} lang:en -is:retweet -is:reply -is:quote -has:mentions`,
     max_results: limit,
   });
   return result
